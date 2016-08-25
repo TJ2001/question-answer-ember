614b7661eb7e6c9a1eaf786eba8c6b1c3406b504
TJ2001 <jung.m.tim@gmail.com>
2016-08-25 08:22:30 -0700

    add edits to index to not error

--- app/models/question.js
+++ app/models/question.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  screenName: DS.attr(),
+  avatar: DS.attr(),
+  query: DS.attr(),
+  content: DS.attr(),
+  answers: DS.hasMany('answer', {async:true})
 });
